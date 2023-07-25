61037c201915d875b75ad525e812be003327d14f
Sarthak <sarthakvengurlekar10@gmail.com>
2023-07-25 10:35:12 -0700

    Created a result and an error html page

--- app.py
+++ app.py
@@ -25,23 +25,23 @@ def convert():
         target_unit = data['target_unit'].lower()
         student_response = data['student_response']
     except KeyError as e:
-        return jsonify({'error': f'Missing field: {e.args[0]}'}), 400
+        return render_template('error.html', error=f'Missing field: {e.args[0]}')
     except ValueError:
-        return jsonify({'error': 'Invalid numerical value'}), 400
+        return render_template('error.html', error='Invalid numerical value')
 
     if not (is_number(input_value) and is_number(student_response)):
-        return jsonify({'error': 'Invalid input, numerical values required'}), 400
+        return render_template('error.html', error='Invalid input, numerical values required')
 
     if input_unit not in ['kelvin', 'celsius', 'fahrenheit', 'rankine'] or target_unit not in ['kelvin', 'celsius', 'fahrenheit', 'rankine']:
-        return jsonify({'error': 'Invalid unit, must be one of: kelvin, celsius, fahrenheit, rankine'}), 400
+        return render_template('error.html', error='Invalid unit, must be one of: kelvin, celsius, fahrenheit, rankine')
 
     correct_answer = round(convert_temperature(input_value, input_unit, target_unit), 1)
     student_answer = round(float(student_response), 1)
 
     if student_answer == correct_answer:
-        return jsonify({'result': 'correct'})
+        return render_template('result.html', result='Correct')
     else:
-        return jsonify({'result': 'incorrect'})
+        return render_template('result.html', result='Incorrect')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
